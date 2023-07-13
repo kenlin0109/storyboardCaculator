9c5d214425bf225d9eaf01b26000a0ebc4ecb1ee
kenlin0109 <51696201+kenlin0109@users.noreply.github.com>
2023-07-13 20:51:14 +0800

    0.91 change variable name & label name

--- storyboard/ViewController.swift
+++ storyboard/ViewController.swift
@@ -8,119 +8,119 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var labOperator = ""
+    var strOperator = ""
     var cn = ""
     var pn = ""
     @IBAction func percentClick(_ sender: UIButton) {
-        if let nNumber = labelNumber.text {
+        if let nNumber = labelCurrentNumber.text {
             let nFloat = (Float(nNumber) ?? 0) * 0.01
-            labelNumber.text = String(format: "%g", nFloat)
+            labelCurrentNumber.text = String(format: "%g", nFloat)
             
         }
     }
     
     @IBAction func plusMinusClick(_ sender: UIButton) {
-        if labelNumber.text == "" {
+        if labelCurrentNumber.text == "" {
             return
         }
-        let number = labelNumber.text ?? ""
+        let number = labelCurrentNumber.text ?? ""
         if number.prefix(1) != "-" {
-            labelNumber.text = "-" + number
+            labelCurrentNumber.text = "-" + number
         } else {
             let worldIndex = number.index(number.startIndex, offsetBy: 1)
-            labelNumber.text = String(number.suffix(from: worldIndex))
+            labelCurrentNumber.text = String(number.suffix(from: worldIndex))
         }
     }
     
     @IBAction func ACClick(_ sender: UIButton) {
-        labelNumber.text = ""
-        labOperator = ""
-        preNumber.text = ""
+        labelCurrentNumber.text = ""
+        strOperator = ""
+        labelPreNumber.text = ""
     }
     
     @IBAction func divisionClick(_ sender: UIButton) {
-        if labOperator == "" {
-            preNumber.text = labelNumber.text ?? ""
-            labelNumber.text = ""
+        if strOperator == "" {
+            labelPreNumber.text = labelCurrentNumber.text ?? ""
+            labelCurrentNumber.text = ""
         } 
-        labOperator = "/"
+        strOperator = "/"
     }
     
     @IBAction func multipleClick(_ sender: UIButton) {
-        if labOperator == ""{
-            preNumber.text = labelNumber.text ?? ""
-            labelNumber.text = ""
+        if strOperator == ""{
+            labelPreNumber.text = labelCurrentNumber.text ?? ""
+            labelCurrentNumber.text = ""
         }
-        labOperator = "*"
+        strOperator = "*"
     }
     
     @IBAction func minusClick(_ sender: UIButton) {
-        if labOperator == "" {
-            preNumber.text = labelNumber.text ?? ""
-            labelNumber.text = ""
+        if strOperator == "" {
+            labelPreNumber.text = labelCurrentNumber.text ?? ""
+            labelCurrentNumber.text = ""
         }
-        labOperator = "-"
+        strOperator = "-"
     }
     
     @IBAction func plusClick(_ sender: UIButton) {
-        if labOperator == "" {
-            preNumber.text = labelNumber.text ?? ""
-            labelNumber.text = ""
+        if strOperator == "" {
+            labelPreNumber.text = labelCurrentNumber.text ?? ""
+            labelCurrentNumber.text = ""
         }
-        labOperator = "+"
+        strOperator = "+"
     }
     
     @IBAction func equalsClick(_ sender: UIButton) {
-        if labelNumber.text == "" {
+        if labelCurrentNumber.text == "" {
             return
         }
-        if labOperator == "+" {
-            if let ntext = labelNumber.text {
+        if strOperator == "+" {
+            if let ntext = labelCurrentNumber.text {
                 let cn = Int(ntext)
-                if let ptext = preNumber.text {
+                if let ptext = labelPreNumber.text {
                     let pn = Int(ptext)
                     let tn = (pn ?? 0) + (cn ?? 0)
-                    labelNumber.text = String(tn)
-                    preNumber.text = ""
-                    labOperator = ""
+                    labelCurrentNumber.text = String(tn)
+                    labelPreNumber.text = ""
+                    strOperator = ""
                 }
             }
         }
         
-        if labOperator == "-" {
-            if let ntext = labelNumber.text {
+        if strOperator == "-" {
+            if let ntext = labelCurrentNumber.text {
                 let cn = Int(ntext)
-                if let ptext = preNumber.text {
+                if let ptext = labelPreNumber.text {
                     let pn = Int(ptext)
                     let tn = (pn ?? 0) - (cn ?? 0)
-                    labelNumber.text = String(tn)
-                    preNumber.text = ""
-                    labOperator = ""
+                    labelCurrentNumber.text = String(tn)
+                    labelPreNumber.text = ""
+                    strOperator = ""
                 }
             }
         }
         
-        if labOperator == "*" {
-            if let ntext = labelNumber.text {
+        if strOperator == "*" {
+            if let ntext = labelCurrentNumber.text {
                 let cn = Int(ntext)
-                if let ptext = preNumber.text {
+                if let ptext = labelPreNumber.text {
                     let pn = Int(ptext)
                     let tn = (pn ?? 0) * (cn ?? 0)
-                    labelNumber.text = String(tn)
-                    preNumber.text = ""
-                    labOperator = ""
+                    labelCurrentNumber.text = String(tn)
+                    labelPreNumber.text = ""
+                    strOperator = ""
                 }
             }
         }
-        if labOperator == "/" {
-            if let ntext = labelNumber.text {
+        if strOperator == "/" {
+            if let ntext = labelCurrentNumber.text {
                 let cn = Int(ntext)
-                if let ptext = preNumber.text {
+                if let ptext = labelPreNumber.text {
                     let pn = Int(ptext)
                     let tn = (pn ?? 0) / (cn ?? 0)
-                    labelNumber.text = String(tn)
-                    preNumber.text = ""
-                    labOperator = ""
+                    labelCurrentNumber.text = String(tn)
+                    labelPreNumber.text = ""
+                    strOperator = ""
                 }
             }
         }
@@ -128,23 +128,23 @@ class ViewController: UIViewController {
     
     @IBAction func digitClick(_ sender: UIButton) {
         let title = sender.titleLabel?.text ?? ""
-        labelNumber.text = (labelNumber?.text ?? "") + title
+        labelCurrentNumber.text = (labelCurrentNumber?.text ?? "") + title
     }
     
     @IBAction func dotClick(_ sender: UIButton) {
-        if labelNumber.text == "" || labelNumber.text?.ranges(of: ".").count == 0 {
-            labelNumber.text = (labelNumber?.text ?? "") + "."
+        if labelCurrentNumber.text == "" || labelCurrentNumber.text?.ranges(of: ".").count == 0 {
+            labelCurrentNumber.text = (labelCurrentNumber?.text ?? "") + "."
         }
             
     }
     
-    @IBOutlet weak var preNumber: UILabel!
-    @IBOutlet weak var labelNumber: UILabel!
+    @IBOutlet weak var labelPreNumber: UILabel!
+    @IBOutlet weak var labelCurrentNumber: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        labelNumber.text = ""
-        preNumber.text = ""
+        labelCurrentNumber.text = ""
+        labelPreNumber.text = ""
         // Do any additional setup after loading the view.
     }
 
